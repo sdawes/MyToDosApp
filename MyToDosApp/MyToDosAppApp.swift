bf03ae2f1f1f98dd3a4c14437342734cf1e88ee0
Stephen Dawes <stephenrdawes@gmail.com>
2024-04-08 21:12:21 +0100

    Initial Commit

---
+++ MyToDosApp/MyToDosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosAppApp.swift
+//  MyToDosApp
+//
+//  Created by Stephen Dawes on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
