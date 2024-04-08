83b1b2c8675f78a4f94a7fc9e917b525555c6f2b
Stephen Dawes <stephenrdawes@gmail.com>
2024-04-08 21:35:43 +0100

    Initial commit

--- MyToDosApp/ToDoItem.swift
+++ MyToDosApp/ToDoItem.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class ToDoItem {
+    var title: String
+    var timestamp: Date
+    var isCritical: Bool
+    var isCompleted: Bool
+    
+    init(title: String = "", timestamp: Date = .now, isCritical: Bool = false, isCompleted: Bool = false) {
+        self.title = title
+        self.timestamp = timestamp
+        self.isCritical = isCritical
+        self.isCompleted = isCompleted
+    }
+}
